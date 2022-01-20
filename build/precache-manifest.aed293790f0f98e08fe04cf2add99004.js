7e0e98ac7951330b7a8e3c48fd79c60e33468ff0
THIRAPHAT B0ATY <79009861+Thiraphat-DEV@users.noreply.github.com>
2022-01-20 11:57:30 +0700

    Lab12

---
+++ build/precache-manifest.aed293790f0f98e08fe04cf2add99004.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c724945f16f78ad7fd76",
+    "url": "/static/js/main.3e9082c0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8e63ac370afd1c64c166",
+    "url": "/static/js/2.e1e3acdb.chunk.js"
+  },
+  {
+    "revision": "17f62aa17f574b8247e395858148febe",
+    "url": "/index.html"
+  }
+];
